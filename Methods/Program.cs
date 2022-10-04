823629dc1191c27c2fdb17897a8a74ec13f612a3
ThatRandomGamer1337 <113189409+ThatRandomGamer1337@users.noreply.github.com>
2022-10-04 08:22:41 -0300

    Extra Changes + Clarifications

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -18,6 +18,10 @@ namespace Methods
 
             // - - - - - - - - - - - - - - - - - - - - >
 
+            // This little bit holds all the data used throughout the code, like the score and health and the points values.
+
+            // < Interger storage >
+
             score = 0;
             health = 3;
 
@@ -39,75 +43,77 @@ namespace Methods
             int triplekill;
             triplekill = 3;
 
-            // < Multipliers >
-            int healthboost;
-            healthboost = 1;
-
             // - - - - - - - - - - - - - - - - - - - - >
+
+
 
             // - Simulated Gameplay - >
 
             ShowHUD();
 
-            AddScore(enemy1Points, 1);
+            AddScore(enemy1Points, 1); // Enemy Killed
             ShowHUD();
 
             AddScore(enemy2Points, 1);
-            AddScore(enemy1Points, doublekill);
+            AddScore(enemy1Points, doublekill); // 2 Enemies killed in a row in under a few seconds, so bonus points given.
             ShowHUD();
 
-            DoubleKillNotif();
-            DamageSystem(1);
+            DoubleKillNotif(); // Player is told they got a double kill.
+            DamageSystem(1); // Player takes damage.
             ShowHUD();
 
-            DamageNotif();
+            DamageNotif(); // Player is told they took damage.
             ShowHUD();
 
             AddScore(enemy1Points, 1);
             AddScore(enemy3Points, doublekill);
-            AddScore(enemy1Points, triplekill);
+            AddScore(enemy1Points, triplekill); // 3 Enemies killed in a row in under a few seconds, so MORE bonus points given.
             ShowHUD();
 
             DoubleKillNotif();
-            TripleKillNotif();
+            TripleKillNotif(); // Player is told they got a triple kill.
+            ShowHUD();
+
+            DamageSystem(-1); // Player regains health.
             ShowHUD();
             Console.ReadKey(true);
 
             // - Simulated Gameplay - >
+
         }
-        static void ShowHUD() // can put stuff in here for declaration stuff and simplification
+        static void ShowHUD() // can put stuff in here for declaration stuff and simplification.
         {
             Console.WriteLine("[=- - - - - -=]");
-            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Score: " + score);    // This is the HUD itself.
             Console.WriteLine("Lives: " + health);
             Console.WriteLine("[=- - - - - -=]");
             Console.WriteLine();
 
         }
 
-        static void DamageSystem(int damage)
+        static void DamageSystem(int damage) // This is the method for when damage is take. This is also used to heal players if they regain health.
         {
             health = health - damage;
         }
 
-        static void DamageNotif()
+        static void DamageNotif() // This pops up whenever you take damage.
         {
             Console.WriteLine("<> Life Lost! Watch yourself! <>");
             Console.WriteLine();
         }
 
-        static void AddScore(int points, int multiplyer)
+        static void AddScore(int points, int multiplyer) // This calulates the points you get per kill + whenever you get double or triple kills on what points to add.
         {
             score = score + points;
             score = score * multiplyer;
         }
 
-        static void DoubleKillNotif()
+        static void DoubleKillNotif() // This tells you when you get a double kill.
         {
             Console.WriteLine("< Double Kill! >");
             Console.WriteLine();
         }
-        static void TripleKillNotif()
+        static void TripleKillNotif() // This tells you when you get a triple kill.
         {
             Console.WriteLine("< Triple Kill! >");
             Console.WriteLine();
